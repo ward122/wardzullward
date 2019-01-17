a717ea5f92228ad958bc19a2a14a8c6a5312a233
ward122 <45214339+ward122@users.noreply.github.com>
2019-01-17 16:04:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "+";
 var adminprefix = '#'
 
 
-const developers = ["ID","ID"]
+const developers = ["513232795901100037"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
